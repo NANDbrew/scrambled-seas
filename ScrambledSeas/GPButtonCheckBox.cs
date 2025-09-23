5b46e47a6e1b695667f94d4fb612ee6976c92f1b
NANDbrew <zortman@gmail.com>
2025-09-22 21:45:22 -0500

    improved/expanded world recreation from file menu

--- ScrambledSeas/GPButtonCheckBox.cs
+++ ScrambledSeas/GPButtonCheckBox.cs
@@ -133,26 +133,48 @@ namespace ScrambledSeas
             ScrambledSeasSaveContainer fromFile = SaveFileHelper.Load<ScrambledSeasSaveContainer>("ScrambledSeas");
             string filename = $"scramble_{SaveSlots.currentSlot}.xml";
             string errorMessage = "";
-            if (fromFile.version <= 0)
+            string message = "";
+
+            if (fromFile.version > 0)
+            {
+                if (fromFile.worldScramblerSeed == 0)
+                {
+                    message = "island positions will be loaded directly\n";
+                }
+                else
+                {
+                    message = $"scramble will be recreated from seed: {fromFile.worldScramblerSeed}\n";
+                }
+                if (fromFile.version != WorldScrambler.version)
+                {
+                    error = true;
+                    errorMessage = "this file is not compatible with this\nversion of Scrambled Seas";
+                    message = "";
+                }
+                else if (fromFile.borderExpander == 1 && !Main.borderExpander)
+                {
+                    if (fromFile.worldScramblerSeed == 0)
+                    {
+                        errorMessage = "this file was made with Border Expander\nsome islands may be inaccessible";
+                    }
+                    else
+                    {
+                        error = true;
+                        errorMessage = "this file requires Border Expander";
+                    }
+
+                }
+            }
+            else
             {
                 error = true;
                 //text.text = "<color=#660000>--</color>";
                 filename = $"<color=#660000>{filename}</color>";
                 errorMessage = "file not found";
             }
-            else if (fromFile.version != WorldScrambler.version)
-            {
-                error = true;
-                errorMessage = "file is not compatible with this version of Scrambled Seas";
-            }
-            else if (fromFile.borderExpander == 1 && !Main.borderExpander)
-            {
-                error = true;
-                errorMessage = "scramble was made with Border Expander\nsome islands may be inaccessible";
-            }
-
+            errorMessage = $"<color=#660000>{errorMessage}</color>";
             Patches.StartMenuPatch.scramblerUI.Find("controls/load_options/filename").GetComponent<TextMesh>().text = filename;
-            Patches.StartMenuPatch.scramblerUI.Find("controls/load_options/error_text").GetComponent<TextMesh>().text = errorMessage;
+            Patches.StartMenuPatch.scramblerUI.Find("controls/load_options/error_text").GetComponent<TextMesh>().text = message + errorMessage;
             //Patches.StartMenuPatch.scramblerUI.Find("controls/load_options/seed").GetComponent<TextMesh>().text = seed;
             Patches.StartMenuPatch.scramblerUI.Find("controls/load_options/file_scale/file_scale_num").GetComponent<TextMesh>().text = $"{((float)fromFile.minArchipelagoSeparation / 30000).ToString("0.0")}x\n\n{((float)fromFile.islandSpread / 10000).ToString("0.0")}x";
 
