6b3703a0c57e3ef4fce3152c5cb1c735bd9368c4
NANDbrew <zortman@gmail.com>
2025-11-04 01:36:13 -0600

    bump version number

--- ScrambledSeas/BepInExMain.cs
+++ ScrambledSeas/BepInExMain.cs
@@ -22,7 +22,7 @@ namespace ScrambledSeas
     {
         public const string GUID = "com.nandbrew.scrambledseas";
         public const string NAME = "Scrambled Seas: NAND edition";
-        public const string VERSION = "7.1.7";
+        public const string VERSION = "7.1.8";
 
         internal static ConfigEntry<bool> random_Enabled;
         //internal static ConfigEntry<bool> hideDestinationCoords_Enabled;
